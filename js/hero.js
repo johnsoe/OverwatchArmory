17ad1ef47910ef5350abfc944bb24e519f2ac03f
johnsoe <evan.johnson.r@gmail.com>
2016-06-23 19:31:24 -0700

    Update html with script references

--- js/hero.js
+++ js/hero.js
@@ -1,12 +1,7 @@
+
+//TODO: Add abilities
 var Hero = function() {
 	this.name = "";
-	this.abilities = [];
 	this.portrait = "";
 	this.type = "";
-};
-
-var Ability = function() {
-	this.name = "";
-	this.description = "";
-	this.icon = "";
 };
--- js/requests.js
+++ js/requests.js
@@ -1,10 +1,21 @@
 (function() {
 
-	var heroes = [];
-
-	var getAllHeroes = function() {
+	var getAllHeroes = function(callback) {
 		firebase.database().ref('/').once('value').then(function(snapshot) {
-		 	var value = snapshot.val();
+		 	callback(snapshot.val());
 		});
 	};
+
+	var getHeroPortrait = function(heroName, callback) {
+		firebase.storage().child('portraits/' + heroName + ".png").getDownloadUrl().then(function(url) {
+			callback(url);
+		}).catch(function(error) {
+			console.log(error);
+		});
+	};
+
+	var heroes = [];
+	getAllHeroes(function(jsonHeroes) {
+		//TODO: parse jsonHeroes and populate Hero objects from data. 
+	});
 })();
