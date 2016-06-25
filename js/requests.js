5f4a8690f7b86ea1a697c602ed46c9b978c4cad5
johnsoe <evan.johnson.r@gmail.com>
2016-06-25 16:50:51 -0700

    Modify function to query for all heroes.

--- js/requests.js
+++ js/requests.js
@@ -6,16 +6,23 @@
 		});
 	};
 
-	var getHeroPortrait = function(heroName, callback) {
-		firebase.storage().child('portraits/' + heroName + ".png").getDownloadUrl().then(function(url) {
+	var getImage = function(img, callback) {
+		firebase.storage().child("portraits/" + img).getDownloadUrl().then(function(url) {
 			callback(url);
 		}).catch(function(error) {
 			console.log(error);
 		});
-	};
+	};	
 
 	var heroes = [];
 	getAllHeroes(function(jsonHeroes) {
-		//TODO: parse jsonHeroes and populate Hero objects from data. 
+		jsonHeroes.forEach(function(jsonHero) {
+			var hero = new Hero();
+			hero.name = jsonHero.name;
+			hero.portrait = hero.name + "_portrait.png";
+			hero.type = jsonHero.type;
+			heroes.push(hero);
+		});
+		console.log(heroes);
 	});
 })();
