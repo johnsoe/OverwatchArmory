49570fac5d3f9cb9e254b71797a1efdbcca2e3c3
johnsoe <evan.johnson.r@gmail.com>
2016-07-04 16:57:34 -0700

    Update requests and how downloadUrls are applied

--- js/heroController.js
+++ js/heroController.js
@@ -15,7 +15,6 @@ Heroes.controller('HeroesController', ['$scope', function($scope) {
 			$scope.heroes.push(hero);
 		});
 		console.log($scope.heroes);
-		$scope.$apply();
 		getImagesForHeroes();
 	});
 
@@ -23,9 +22,9 @@ Heroes.controller('HeroesController', ['$scope', function($scope) {
 		$scope.heroes.forEach(function(hero) {
 			requests.getImage(hero.portrait, function(downloadUrl) {
 				$scope.downloadUrls[hero.name] = downloadUrl;
+				console.log("DownloadUrl: " + downloadUrl);
+				$scope.$apply();
 			});
 		});
-		console.log($scope.downloadUrls);
-		$scope.$apply();
 	}
 }]);
